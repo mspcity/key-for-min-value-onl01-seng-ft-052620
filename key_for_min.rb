7c0ddf7619111c964ef8987aadbf8684a3c0b88f
Youngmin Park <codepark2019@gmail.com>
2020-06-02 18:17:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,7 +10,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
 
     if min_value == 0 || value < min_value
-      binding.pry
+      
       min_value = value
       mkey = key
     end
